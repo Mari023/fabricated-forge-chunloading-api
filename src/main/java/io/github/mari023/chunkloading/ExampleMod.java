bedddc36c2ac35a9f768724ddab7d878a7839445
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-07-25 03:37:53 +0200

    it works (at least with ae2)

--- src/main/java/io/github/mari023/chunkloading/ExampleMod.java
+++ src/main/java/io/github/mari023/chunkloading/ExampleMod.java
@@ -1,11 +1,8 @@
 package io.github.mari023.chunkloading;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
 	public void onInitialize() {
